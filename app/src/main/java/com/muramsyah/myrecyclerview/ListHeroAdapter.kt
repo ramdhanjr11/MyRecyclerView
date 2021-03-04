c215d30dc3597d0a3f258e8d7d0e6265d93b3652
ramdhanjr11 <ramdhanjr690@gmail.com>
2021-03-04 22:01:48 +0700

    Added Model, Adapter, and a function to showRecyclerList in MainActivity.kt

--- app/src/main/java/com/muramsyah/myrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/muramsyah/myrecyclerview/ListHeroAdapter.kt
@@ -6,6 +6,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
 
@@ -21,7 +23,13 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val hero = listHero[position]
 
+        Glide.with(holder.itemView.context)
+            .load(hero.photo)
+            .apply(RequestOptions().override(55, 55))
+            .into(holder.imgPhoto)
 
+        holder.tvName.text = hero.name
+        holder.tvDetail.text = hero.detail
     }
 
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
